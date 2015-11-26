fdfbf0991b8467eca713cb2ee7e56283107bacf7
danharel <cheezzywizz@gmail.com>
2015-11-26 00:20:09 -0500

    Added main function to server code

--- server.py
+++ server.py
@@ -3,21 +3,25 @@ from socket import *
 # Assign the ephemeral port to the server
 serverPort = 0
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+def main():
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('', serverPort))
+    serverSocket.listen(1)
 
-print "Server started with address %s and port %d" % serverSocket.getsockname()
-print serverSocket.getsockname()
+    print "Server started with address %s and port %d" % serverSocket.getsockname()
+    print serverSocket.getsockname()
 
-while True:
-    print 'Ready to serve...'
-    connectionSocket, addr = serverSocket.accept()
-    data = connectionSocket.recv(1024)
-    print "Received data: " + data
+    while True:
+        print 'Ready to serve...'
+        connectionSocket, addr = serverSocket.accept()
+        data = connectionSocket.recv(1024)
+        print "Received data: " + data
 
-    if data == 'exit server':
-        break
+        if data == 'exit server':
+            break
 
-print "Exiting..."
-serverSocket.close()
+    print "Exiting..."
+    serverSocket.close()
+
+if __name__ == '__main__':
+    main()
